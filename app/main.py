1837ce3d02a15ca49695323b7fda15d71c8c2ebe
Kyrylo Cherdakliiev <yungkeryaa@gmail.com>
2025-12-11 18:02:50 +0100

    Implemented file copying

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split()
+    if len(split_command) == 3 and split_command[0] == "cp":
+        source_file = split_command[1]
+        copied_file = split_command[2]
+        if source_file and not source_file.endswith(".txt"):
+            source_file += ".txt"
+        try:
+            if source_file != copied_file:
+                with (open(source_file, "r") as file_in,
+                      open(copied_file, "w") as file_out):
+                    file_out.write(file_in.read())
+        except FileNotFoundError:
+            print("File does not exist")
+    else:
+        print("There are a problem with a command!")
